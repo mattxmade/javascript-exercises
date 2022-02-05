56b6e8fe08b57bd1ccc344341a720d3ad378772a
Matt <97047080+mattxmade@users.noreply.github.com>
2022-02-05 16:53:09 +0000

    Completed removeFromArray(): Passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...params) {
 
-};
+  for (let i=0; i < params.length; i++) {
+
+    array.filter((item, index) => {
+
+      if (item === params[i]) {
+        array.splice(index, 1);
+      }
+    });
+  }
+
+  return array;
+}
+
+// myTests
+//removeFromArray([1, 9, 10, "tacos", "yo"], 9, 10, "tacos", -6);
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// multiple parameters | https://stackoverflow.com/a/45917018
