d9a1f7f9b9910e607238056acb8ae2a6aa4f9030
Matt <97047080+mattxmade@users.noreply.github.com>
2022-02-05 14:07:55 +0000

    Function now returns ERROR if num is negative

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,17 +2,33 @@ const repeatString = function(string, number) {
 
   let stringMultlplier = '';
 
-  for (let i = 0; i < number; ++i) {
-    stringMultlplier += string;
+  if (number < 0) {
+    stringMultlplier = 'ERROR';
   }
+
+  else {
+    for (let i = 0; i < number; ++i) {
+      stringMultlplier += string;
+    }
+  }
+
   return stringMultlplier;
 };
 
-// positive numbers
+// positive numbers         | PASS
 repeatString('hey', 3);
 repeatString('hey', 10);
 repeatString('hey', 1);
 repeatString('hey', 0);
+
+// negative numbers         | PASS
+repeatString('hey', -1);
+
+// random                   | Pass
+// blank string             | Pass
+
+// ALL PASSED
+
 
 // Do not edit below this line
 module.exports = repeatString;
