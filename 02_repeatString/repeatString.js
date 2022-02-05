3616a59979692de4e5463bfe31145b9af8f18ec6
Matt <97047080+mattxmade@users.noreply.github.com>
2022-02-05 13:49:56 +0000

    Pass: Function working with positive numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
 
+  let stringMultlplier = '';
+
+  for (let i = 0; i < number; ++i) {
+    stringMultlplier += string;
+  }
+  return stringMultlplier;
 };
+
+// positive numbers
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
 
 // Do not edit below this line
 module.exports = repeatString;
