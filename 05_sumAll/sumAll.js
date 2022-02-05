fe9f4fecb261ec0e3ad6a507fbfce7191a236334
Matt <97047080+mattxmade@users.noreply.github.com>
2022-02-05 18:39:40 +0000

    Completed function sumAll: TDD PASS

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,36 @@
-const sumAll = function() {
+const sumAll = function(numX, numY) {
 
+  if (numX < 0 || numY < 0) {
+    return 'ERROR';
+  }
+
+  if (typeof numX === 'number' && typeof numY === 'number') {
+
+    let highestNumber = 0;
+    let lowestNumber  = 0;
+
+    if (numX >= numY) {
+      highestNumber = numX;
+      lowestNumber = numY;
+    }
+    else {
+      highestNumber = numY;
+      lowestNumber = numX;
+    }
+
+    let summedUp = 0;
+
+    for (let i = 0; i < highestNumber;) {
+
+      summedUp += lowestNumber + i;
+      highestNumber > 0 ? i++ : i--; 
+
+    }
+
+    return summedUp;
+  }
+
+  else { return 'ERROR'; }
 };
 
 // Do not edit below this line
