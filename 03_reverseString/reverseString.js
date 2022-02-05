04c50099bc85719a4f5ce4d1f7e43717932c60db
Matt <97047080+mattxmade@users.noreply.github.com>
2022-02-05 15:44:30 +0000

    Completed function; reverseString(): Test pass success

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let stringReverse = [];
 
+  for (let i = 0; i < string.length; i++) {
+    stringReverse.unshift(string[i]);
+  }
+  stringReverse = stringReverse.join('').toString();
+  return stringReverse;
 };
+
+// log test
+//console.log(reverseString('This is the same but backwards!'));
 
 // Do not edit below this line
 module.exports = reverseString;
+
+//join() array method :: https://stackoverflow.com/a/12132212
