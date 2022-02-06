221bbe6008e3febae9e73b783f660475ffc62691
Matt <97047080+mattxmade@users.noreply.github.com>
2022-02-06 14:03:16 +0000

    Completed leapYears function: script passed 6/6 tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+  if ( year % 400 === 0 && (year % 100 === 0) ) {
+    console.log(`${year} is leap year!`);
+    return true;
+  }
+  
+  else if ( year % 100 === 0 ) {
+    console.log(`${year} is not leap year!`);
+    return false;
+  }
+
+  else if ( year % 4 === 0 ) {
+    console.log(`${year} is a leap year!`);
+    return true;
+  }
+
+  else { return false; }
 
 };
 
