a5826df092f4a3ab3ca9051f7c338c740596b3ff
Matt <97047080+mattxmade@users.noreply.github.com>
2022-02-06 13:41:02 +0000

    Refactored function: uniform var names; moved increment into for ()

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,29 +1,28 @@
 const sumAll = function(numX, numY) {
 
-  if (numX < 0 || numY < 0) {
+  if ( numX < 0 || numY < 0 ) {
     return 'ERROR';
   }
 
   if (typeof numX === 'number' && typeof numY === 'number') {
 
-    let highestNumber = 0;
-    let lowestNumber  = 0;
+    let hiNumber = 0;
+    let loNumber  = 0;
 
     if (numX >= numY) {
-      highestNumber = numX;
-      lowestNumber = numY;
+      hiNumber = numX;
+      loNumber = numY;
     }
     else {
-      highestNumber = numY;
-      lowestNumber = numX;
+      hiNumber = numY;
+      loNumber = numX;
     }
 
     let summedUp = 0;
 
-    for (let i = 0; i < highestNumber;) {
+    for (let i = 0; i < hiNumber; i++) {
 
-      summedUp += lowestNumber + i;
-      highestNumber > 0 ? i++ : i--; 
+      summedUp += loNumber + i;
 
     }
 
