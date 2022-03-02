2f9ea0847c2d4a15543ade618e4582c0eaa6f009
Matt <97047080+mattxmade@users.noreply.github.com>
2022-03-02 18:13:54 +0000

    Completed functions: Utilised .reduce() method

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(numX, numY) {
+	return numX + numY;
 };
 
-const subtract = function() {
-	
+const subtract = function(numX, numY) {
+	return numX - numY;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.length > 0 ? array.reduce((prevVal, currVal) => prevVal + currVal) : 0;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((numX, numY) => numX * numY);
 };
 
-const power = function() {
-	
+const power = function(numX, numY) {
+  const powerOf = [];
+
+  for (let i = 0; i < numY; i++) {
+    powerOf.push(numX);
+  }
+
+  return powerOf.reduce((prevVal, currVal) => prevVal * currVal);
+  
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+
+  const factorialOf = [];
+
+  for(let i = num; i > 0; i--) {
+    factorialOf.push(i);
+  }
+
+  return factorialOf.reduce((numX, numY) => numX * numY, 1);
 };
 
 // Do not edit below this line
