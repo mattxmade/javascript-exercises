385cb42e2e7b350acfcb35228302a0a061cb2231
Matt <97047080+mattxmade@users.noreply.github.com>
2022-03-03 12:08:16 +0000

    Completed function: Utilised regex, string and array methods

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+string = string.match(/[^_\W]+/g).join("");             // 1)
+string = string.toLowerCase();
+
+const stringArray = [];
+
+for (let i = 0; i < string.length; i++) {
+  stringArray.push([string.slice(i, i+1)]);
+}
+
+const reverseString = (stringArray.reverse().join(""));
+
+return reverseString === string ? true : false;
 };
+
+// store characters in reverse order
+// letters only using regex
+// compare strings
+// return true if match
+
+// 1) https://stackoverflow.com/a/31047727
 
 // Do not edit below this line
 module.exports = palindromes;
